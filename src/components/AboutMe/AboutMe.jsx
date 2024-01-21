44d7e9bbf012e0031df2be51433e6678194b8610
MorasDeFresa <bencito10@gmail.com>
2024-01-21 08:32:59 -0500

    refactor(AboutMe): add content

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,13 +1,36 @@
 /* eslint-disable react/no-unescaped-entities */
 import "./AboutMe.css";
 import { useLanguage } from "../../hooks/useLanguage";
+import LinkSocialMedia from "../../assets/LinkSocialMedia.json";
 
 const AboutMe = () => {
-  const [t] = useLanguage();
+  const [t, handleChangeLanguage, actualLanguage] = useLanguage();
+  let UrlCV;
+  if (actualLanguage === "es") UrlCV = LinkSocialMedia.Es_CV;
+  else {
+    UrlCV = LinkSocialMedia.En_CV;
+  }
   return (
     <div className="AboutDiv">
       <h2>{t("AboutMe.header")}</h2>
       <p>{t("AboutMe.content")}</p>
+      <div style={{ marginTop: "1.5vh" }}>
+        <button
+          className="btn btn-outline-theme btn-floating m-1"
+          target="blank"
+        >
+          <i className="bi bi-tools"></i>
+          <span> {t("AboutMe.tool")}</span>
+        </button>
+        <a
+          className="btn btn-outline-theme btn-floating m-1"
+          target="blank"
+          href={UrlCV}
+        >
+          <i className="bi bi-file-earmark-person-fill"></i>
+          <span> {t("AboutMe.cv")}</span>
+        </a>
+      </div>
     </div>
   );
 };
